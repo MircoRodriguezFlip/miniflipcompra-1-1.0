e28efae7443f5ec950d5dc30f8c68632f5c82921
mircomania <mirco.rodriguez@hotmail.com>
2025-02-21 15:51:46 -0300

    2 commit  miniflipcompra - section 1 lp 1.0

--- src/components/imports/Section1Lp.jsx
+++ src/components/imports/Section1Lp.jsx
@@ -1,4 +1,8 @@
-import LogoNavbar from '../../assets/images/logo navbar rojo 2.webp';
+import { useState } from 'react';
+
+import mascota from '../../assets/images/mascota-2.webp';
+import icono1 from '../../assets/images/icono-section-1-lp-1.webp';
+import icono2 from '../../assets/images/icono-section-1-lp-2.webp';
 
 const scrollDown = () => {
     const footer = document.querySelector('.footer');
@@ -8,6 +12,19 @@ const scrollDown = () => {
 };
 
 export const Section1Lp = () => {
+    const [textoVandalizada, setTextoVandalizada] = useState(false);
+    const [textoHipotecada, setTextoHipotecada] = useState(false);
+
+    const vandalizada = () => {
+        setTextoVandalizada((prev) => !prev);
+        setTextoHipotecada(false);
+    };
+
+    const hipotecada = () => {
+        setTextoHipotecada((prev) => !prev);
+        setTextoVandalizada(false);
+    };
+
     return (
         <section className="section-1-lp-container">
             <header className="section-1-lp-header">
@@ -20,24 +37,46 @@ export const Section1Lp = () => {
                     <p className="light-text">rápido y seguro</p>
                     <p className="light-text">¡Déjalo en nuestras manos!</p>
                 </div>
-                <div>
-                    <img src={LogoNavbar} alt="" />
+
+                <div className="imagen-mascota">
+                    <img src={mascota} alt="" />
                 </div>
             </div>
 
             <div className="section-1-lp-content-2">
                 <h3 className="light-text">¿Qué tipo de casa quieres vender?</h3>
 
-                <div className="section-1-lp-content-2-cards">
-                    <button className="section-1-lp-content-2-card">
-                        <img src={LogoNavbar} alt="" />
-                        <p className="light-text">Vandalizada</p>
-                    </button>
+                <div>
+                    <div className="section-1-lp-content-2-cards">
+                        <button className="section-1-lp-content-2-card" onClick={vandalizada} title="Haz clic para saber que es una casa vandalizada">
+                            <img src={icono1} alt="" />
+                            <p className="light-text">Vandalizada</p>
+                        </button>
 
-                    <button className="section-1-lp-content-2-card">
-                        <img src={LogoNavbar} alt="" />
-                        <p className="light-text">Hipotecada</p>
-                    </button>
+                        <button className="section-1-lp-content-2-card" onClick={hipotecada} title="Haz clic para saber que es una casa hipotecada">
+                            <img src={icono2} alt="" />
+                            <p className="light-text">Hipotecada</p>
+                        </button>
+                    </div>
+
+                    <div>
+                        {textoVandalizada && (
+                            <div className="section-1-lp-cards-text">
+                                <p className="light-text">
+                                    Una casa vandalizada es aquella que ha sido dañada por actos de vandalismo, como pintura en las paredes, ventanas
+                                    rotas, etc.
+                                </p>
+                            </div>
+                        )}
+
+                        {textoHipotecada && (
+                            <div className="section-1-lp-cards-text">
+                                <p className="light-text">
+                                    Una casa hipotecada es aquella que tiene una deuda pendiente con el banco y/o en alguna institución financiera.
+                                </p>
+                            </div>
+                        )}
+                    </div>
                 </div>
 
                 <div className="section-1-lp-content-2-button">
--- src/components/imports/Section2Lp.jsx
+++ src/components/imports/Section2Lp.jsx
@@ -2,7 +2,7 @@ import LogoNavbar from '../../assets/images/logo navbar rojo 2.webp';
 
 export const Section2Lp = () => {
     return (
-        <section>
+        <section className="section-2-lp-container">
             <header className="section-2-lp-header">
                 <h2 className="bold-text">¡Rápido, fácil y seguro!</h2>
                 <h3 className="light-text">Vende tu casa en 48 horas</h3>
@@ -20,14 +20,10 @@ export const Section2Lp = () => {
                 </div>
 
                 <div>
-                    <p className="light-text">3.- Conozcamos tu casa</p>
+                    <p className="light-text">3.- Conocemos tu casa</p>
                     <img src={LogoNavbar} alt="" />
                 </div>
             </div>
-
-            <span className="div-pag">
-                <hr />
-            </span>
         </section>
     );
 };
